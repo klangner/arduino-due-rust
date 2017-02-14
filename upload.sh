8b710b5aaebd52450ac5940aae63cffd99f75695
Krzysztof Langner <krzysztof@carlsolutions.com>
2017-02-14 15:49:02 +0100

    Renamed project

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@ echo "Build application"
 xargo build
 
 echo "Copy binary"
-arm-none-eabi-objcopy -O binary target/thumbv7m-none-eabi/debug/due-blink target/blink.bin
+arm-none-eabi-objcopy -O binary target/thumbv7m-none-eabi/debug/arduino-due-rust target/firmware.bin
 
 echo "Touch programming port ..."
 stty -F "/dev/ttyACM0" raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
@@ -11,4 +11,4 @@ printf "\x00" > "/dev/ttyACM0"
 echo "Waiting before uploading ..."
 sleep 1
 echo "Uploading ..."
-/home/klangner/bin/bossac/bossac --write --verify --boot -R target/blink.bin
+/home/klangner/bin/bossac/bossac --write --verify --boot -R target/firmware.bin
