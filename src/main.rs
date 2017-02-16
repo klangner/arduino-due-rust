8218a8c0d2b97067ff85b840f6742e4b3d868660
klangner <klangner@gmail.com>
2017-02-16 19:01:33 +0100

    Refactored RTT. Added high level interface to it.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate sam3x;
 pub mod rust_base;
 use sam3x::*;
 use sam3x::pio;
+use sam3x::rtt;
 
 
 #[link_section=".vectors"]
@@ -17,31 +18,19 @@ pub static VECTOR_TABLE: VectorTable =
     };
 
 
-// As the name suggests, this function sleeps for a given number of milliseconds.
-fn sleep_ms(milliseconds: u32) {
-    unsafe {
-        let sleep_until = *TIMER_VALUE_REGISTER + milliseconds;
-        while *TIMER_VALUE_REGISTER < sleep_until {}
-    }
-}
-
 /// Main function connected to the reset handler
 /// Arduino Led is connected to the controller: B, pin: 27
 fn start() -> ! {
     let pb27 = pio::pin(pio::Port::B, 27, pio::Mode::Output).expect("Can't connect to the led");
 
-    unsafe {
-        // Set the timer to a resolution of a millisecond.
-        *TIMER_MODE_REGISTER = 0x00000020;
-    }
+    //We need to initialize timer before we can use it
+    rtt::init_timer();
 
-    // Continuously set and clear output on PB27 (pin 13). This
-    // blinks the Due's built-in LED, which is the single
-    // purpose of this program.
+    // Blink led
     loop {
         pb27.on();
-        sleep_ms(200);
+        rtt::wait(100);
         pb27.off();
-        sleep_ms(800);
+        rtt::wait(100);
     }
 }
