5281a4cbea8aa09d9f063f7cbec730544110ad0d
klangner <klangner@gmail.com>
2017-02-19 13:08:32 +0100

    Implemented button driver with is_pressed function

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ extern crate sam3x;
 
 pub mod rust_base;
 use sam3x::*;
-use sam3x::drivers::led::{Led, Port};
+use sam3x::hardware::peripherals::{Peripheral};
+use sam3x::drivers::led::{Led};
+use sam3x::drivers::button::{Button};
 use sam3x::hardware::rtt::{init_timer, wait_ms};
+use sam3x::hardware::pmc::{enable_peripheral_clk};
 
 
 #[link_section=".vectors"]
@@ -21,14 +24,26 @@ pub static VECTOR_TABLE: VectorTable =
 /// Main function connected to the reset handler
 /// Arduino Led is connected to the controller B, line 27
 fn start() -> ! {
-    let pb27 = Led::connect(Port::B, 27).expect("Wrong pin for led.");
-    // We need to initialize timer before we can use it
-    init_timer();
 
+    // We need to initialize board before we can use it
+    // We need timer
+    init_timer();
+    // We need to enable clock for Port C before we can use it as input
+    enable_peripheral_clk(Peripheral::PioC);
+
+    // Now we can initialize state
+    let led = Led::connect(Peripheral::PioB, 27).expect("Wrong pin for led.");
+    let button = Button::connect(Peripheral::PioC, 22).expect("Wrong pin for button.");
+
+    // We execute functionality in never ending loop
     loop {
-        pb27.on();
-        wait_ms(200);
-        pb27.off();
-        wait_ms(900);
+        led.on();
+        wait_ms(100);
+        if button.is_pressed() {
+            led.on()
+        } else {
+            led.off()
+        };
+        wait_ms(100);
     }
 }
